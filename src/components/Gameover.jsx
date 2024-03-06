27ccefabf0035b5bdfc207716f078d1f237adb2c
GTX155 <kirchoabv@gmail.com>
2024-03-06 20:05:55 +0200

    added gameover glass backdrop

--- src/components/Gameover.jsx
+++ src/components/Gameover.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './Gameover.css'
 
 function Gameover(props) {
     const [endGame, setEndGame] = useState(false);
-
+    
     useEffect(() => {
         if (props.strikeNum === 3) {
             setEndGame(true);
         }
+
     }, [props.strikeNum]);
 
+    // Resets the game to zero by reloading the page
     function playAgain() {
         window.location.reload();
     };
@@ -17,11 +19,14 @@ function Gameover(props) {
     return (
         <>
             {endGame && 
+            <>
             <dialog open={true}>
                 <h1>GAME OVER!</h1>
                 <h1 className='score'>Score:{props.scoreNum}</h1>
                 <button onClick={playAgain}>PLAY AGAIN?</button>
-            </dialog>}
+            </dialog>
+            <div className="glass"></div>
+            </>}
         </>
     );
 }
